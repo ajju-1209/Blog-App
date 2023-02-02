5d16eaff4f05c5cbdef29ef7c3fb55442d735e91
Abhishek Pant <abhishekpant1990@gmail.com>
2023-02-02 19:03:41 +0530

    initials

---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+  return (
+    <div className="post">
+        <div className="image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnvAuOKji-nHQuDLl_SrG_XomAwUdVEgMKBsdWZamP&s" alt='' />
+        </div>
+        <div className="texts">
+          <h2>Bamboo Eating panda</h2>
+          <p className="info">
+            <a className="author">Ajju</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">Recently a panda is being found eating 
+          Bamboo.He was quite relaxed while enjoying his bamboo.</p>
+          </div>
+      </div>
+  );
+}
